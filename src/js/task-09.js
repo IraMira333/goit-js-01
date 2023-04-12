eca9e66d7cdef49bd3f5a61e610ca4105157be56
IraMira333 <irinat333@gmail.com>
2023-04-13 00:24:11 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorName = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+const bodyEl = document.body;
+
+changeColorBtn.addEventListener("click", onchangeColorBtn);
+
+function onchangeColorBtn(event) {
+  colorName.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorName.textContent;
+}
