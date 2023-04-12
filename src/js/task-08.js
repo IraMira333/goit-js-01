689dc889bb51b06bf205e01331f9f03a2681ce0b
IraMira333 <irinat333@gmail.com>
2023-04-13 00:24:07 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElmts = event.currentTarget.elements;
+  const email = formElmts.email.value;
+  const password = formElmts.password.value;
+  if (email === "" || password === "") {
+    alert("All fields must be filled!");
+  }
+  const formData = {
+    email,
+    password,
+  };
+  console.log("Data: ", formData);
+  event.target.reset();
+  //const formData = new FormData(event.curentTarget);
+}
