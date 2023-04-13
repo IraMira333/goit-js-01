cc1b114292da31856f2da971c3928bd6f1a002b1
IraMira333 <irinat333@gmail.com>
2023-04-13 16:01:27 +0300

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  containerDivsEl: document.querySelector("#boxes"),
+  createBtn: document.querySelector("[data-create]"),
+  destroyBtn: document.querySelector("[data-destroy]"),
+  amountOfDiv: document.querySelector("#controls").firstElementChild,
+};
+
+refs.createBtn.addEventListener("click", createBoxes);
+
+refs.destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = refs.amountOfDiv.value;
+  const arrayDivs = [];
+
+  for (let index = 0; index < amount; index += 1) {
+    const divEl = document.createElement("div");
+    const newWidth = 30 + index * 10;
+    const newHeight = 30 + index * 10;
+    divEl.style.width = newWidth + "px";
+    divEl.style.height = newHeight + "px";
+    divEl.style.backgroundColor = getRandomHexColor();
+    console.log(divEl);
+    arrayDivs.push(divEl);
+  }
+
+  refs.containerDivsEl.append(...arrayDivs);
+}
+
+function destroyBoxes() {
+  refs.containerDivsEl.innerHTML = "";
+}
