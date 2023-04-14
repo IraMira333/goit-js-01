78f20e32aa3de4cd32e2d91fa957c42faea01dd7
IraMira333 <irinat333@gmail.com>
2023-04-15 00:43:53 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,7 @@
 const rangeInput = document.querySelector("#font-size-control");
 const textToChange = document.querySelector("#text");
+textToChange.style.fontSize = rangeInput.value + "px";
+console.log(rangeInput.value);
 
 rangeInput.addEventListener("input", onRangeChange);
 function onRangeChange(event) {
