7c72ba05566016098e379da703da5fbd71a76f99
IraMira333 <irinat333@gmail.com>
2023-04-16 23:04:45 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,4 @@
-// const numberOfCategories = document.querySelectorAll(`.item`);
-// console.log(`Number of categories: `, numberOfCategories.length);
-
-// numberOfCategories.forEach((item, index) => {
-//   console.log(`Category: `, item.querySelector(`h2`).textContent);
-//   console.log(`Elements: `, item.querySelectorAll(`li`).length);
-// });
-
-const body = document.body;
-
-const ul = body.children[1];
-console.log(ul);
-const numberOfCategories = Array.from(ul.children);
+const numberOfCategories = document.querySelectorAll(`.item`);
 
 numberOfCategories.forEach((item, index) => {
   console.log(`Category: `, item.firstElementChild.textContent);
